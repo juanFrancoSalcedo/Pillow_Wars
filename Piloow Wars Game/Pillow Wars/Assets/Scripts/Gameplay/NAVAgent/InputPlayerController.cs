c3ec816e3a6c8d8350390acb4c25e87331dbd2fa
Juan Sebastián Franco <62152724+juanFrancoSalcedo@users.noreply.github.com>
2023-05-23 17:28:32 -0500

    Update InputPlayerController.cs 3

--- Piloow Wars Game/Pillow Wars/Assets/Scripts/Gameplay/NAVAgent/InputPlayerController.cs
+++ Piloow Wars Game/Pillow Wars/Assets/Scripts/Gameplay/NAVAgent/InputPlayerController.cs
@@ -38,7 +38,7 @@ namespace PillowWars
 
         public void LongAttack()
         {
-            print("TODO");
+            print("TODO longAttack");
         }
 
         private void OnEnable() => dragController.OnDragged += GetDragPosition;
